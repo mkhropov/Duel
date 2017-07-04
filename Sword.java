906340cf3b2d83184213ebb8ba06877a5e2d45e9
mkhropov <mkhropov@gmail.com>
2017-07-04 19:02:04 +0300

    main: add fencing template
    I _really_ want to make a gui tool to generate AGraphs
    
    Signed-off-by: mkhropov <mkhropov@gmail.com>

--- Sword.java
+++ Sword.java
@@ -1,13 +1,10 @@
-import org.lwjgl.opengl.GL11;
-import java.lang.Math;
+package main;
 
-public class Sword implements Updatable, Drawable {
-	int x0, y0, x, y;
-	LinFunc lx, ly;
-	int d;
-	public static final int DIR_LR = 1;
-	public static final int DIR_RL = -1;
+import dGraph.*;
+
+public class Sword extends dgSword {
 	public int pos;
+	public int ids[10];
 	public static final int POS0 = 0; /* at ready */
 	public static final int POS1 = 1; /* swing attack start */
 	public static final int POS2 = 2; /* pierce attack start */
@@ -19,61 +16,46 @@ public class Sword implements Updatable, Drawable {
 	public static final int POS8 = 8; /* high attack block */
 	public static final int POS9 = 9; /* middle attack block */
 	public static final int POS10 = 10; /* low attack block */
-	float alpha;
-	LinFunc la;
-	float c, s;
-	public static final float ALPHA_0 = (float)(Math.PI / 4.);
-	static int[] model = {	 0,   0,
-							 8,   35,
-							-8,   35,
-							 7,  200,
-							-7,  200,
-							 16, 200,
-							-16, 200,
-							 16, 205,
-							-16, 205,
-							 5,  205,
-							-5,  205,
-							 5,  250,
-							-5,  250, };
 
+	public AnimationGraph ag;
 
-	public Sword(int x, int y, int dir) {
-		this.x0 = x;
-		this.y0 = y;
-		this.x = x;
-		this.y = y;
-		this.d = dir;
-		this.pos = POS0;
-		this.alpha = -Math.PI/3;
-		this.lx = new LinFunc(0, 0.f, 1, 0.f);
-		this.ly = new LinFunc(0, 0.f, 1, 0.f);
-		this.la = new LinFunc(0, 0.f, 1000, 1.f);
-	}
+	public Sword() {
+		super();
+		this.ag = new AnimationGraph();
+		Animation an;
+		an = new Animation(); an.setDescription("Pierce bk");
+		id[0] = ag.addAnimation(an);
+		an = new Animation(); an.setDescription("Pierce fw");
+		id[1] = ag.addAnimation(an);
+		ag.setFollowup(id[0], 0, id[1]);
+		an = new Animation(); an.setDescription("Pierce rtr");
+		id[2] = ag.addAnimation(an);
+		ag.setFollowup(id[1], 0, id[2]);
+		ag.setFollowup(id[2], 0, id[0]);
 
-	public void setAnimation(Animation an) {
-		this.lx = an.x;
-		this.ly = an.y;
-		this.la = an.a;
-	}
+		an = new Animation(); an.setDescription("Swing bk");
+		id[3] = ag.addAnimation(an);
+		ag.setFollowup(id[2], 1, id[3]);
+		an = new Animation(); an.setDescription("Swing fw hi");
+		id[4] = ag.addAnimation(an);
+		ag.setFollowup(id[3], 0, id[4]);
+		an = new Animation(); an.setDescription("Swing fw lo");
+		id[5] = ag.addAnimation(an);
+		ag.setFollowup(id[3], 1, id[5]);
+		an = new Animation(); an.setDescription("Swing rtr hi");
+		id[6] = ag.addAnimation(an);
+		ag.setFollowup(id[4], 0, id[6]);
+		an = new Animation(); an.setDescription("Swing rtr lo");
+		id[7] = ag.addAnimation(an);
+		ag.setFollowup(id[5], 0, id[7]);
 
-	public void update(long t, long dt) {
-		if (!lx.done(t))
-			x = x0 + Math.round(lx.get(t));
-		if (!ly.done(t))
-			y = y0 + Math.round(ly.get(t));
-		if (!la.done(t)) {
-			alpha = la.get(t);
-			c = (float)Math.cos(d*alpha);
-			s = (float)Math.sin(d*alpha);
-		}
 	}
 
 	public void draw() {
-		GL11.glBegin(GL11.GL_TRIANGLE_STRIP);
-		for (int i = 0; i < model.length; i+=2)
-			GL11.glVertex2d(x+model[i]*c-model[i+1]*s,
-							y+model[i+1]*c+model[i]*s);
-		GL11.glEnd();
+		super.draw();
+	}
+
+	public void update(long t, long dt) {
+		super.update(t, dt);
 	}
 }
